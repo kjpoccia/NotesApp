8436c7a6fc49c13a880c0a7abae539b777fed299
kjpoccia <kjpsubs@gmail.com>
2024-11-12 06:13:42 -0500

    uibuild

--- app.js
+++ app.js
@@ -20,13 +20,14 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connection to MongoDB:', error.message)
   })
 
-app.use('/api/users', usersRouters)
+
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
