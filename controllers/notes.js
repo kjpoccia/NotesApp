b10216ceeeeb21e12ffb5bb0d3b61b9527b5db3f
kjpoccia <kjpsubs@gmail.com>
2024-11-13 12:58:20 -0500

    uibuild

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,7 +1,15 @@
-
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 notesRouter.get('/', (request, response) => {
   Note.find({}).then(notes => {
@@ -23,8 +31,11 @@ notesRouter.get('/:id', (request, response, next) => {
 
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
